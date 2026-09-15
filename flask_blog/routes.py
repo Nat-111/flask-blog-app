f3613124354d79c4dddb690d95c51bfc3121f6d1
Nat-111 <agbodzinathaniel@gmail.com>
2026-09-15 06:56:03 +0000

    User Authentication

--- flask_blog/routes.py
+++ flask_blog/routes.py
@@ -1,7 +1,9 @@
-from flask import render_template, url_for,flash,redirect
+from flask import render_template, url_for,flash,redirect,request
+from wtforms.validators import email
 from flask_blog.forms import RegistrationForm, LoginForm
-from flask_blog import app
+from flask_blog import app, db, bcrypt
 from flask_blog.models import User, Post
+from flask_login import login_user, current_user, logout_user, login_required
 
 
 
@@ -35,10 +37,18 @@ def about():
 
 @app.route("/register",methods=['GET','POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegistrationForm() 
     if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}!', 'success')
-        return redirect(url_for('home'))
+        hash_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        # we create a new instance of the usuer
+        user = User(username=form.username.data, email=form.email.data, password=hash_password)
+        #Adding the user to the database
+        db.session.add(user)
+        db.session.commit()
+        flash(f'Account created you can login in now!', 'success')
+        return redirect(url_for('login'))
 
 
     return render_template('register.html', form=form ,title="Register")
@@ -47,12 +57,29 @@ def register():
 
 @app.route("/login", methods=['GET','POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == "nathaniel@gmail.com" and form.password.data == "password":
-            flash(f'Logged in as {form.email.data}!', 'success')
-            return redirect(url_for('home'))
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+           login_user(user, remember=form.remember.data)
+           next_page = request.args.get('next')
+           return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
             flash('Login Unsuccessful. Please check email and password', 'danger')
 
     return render_template('login.html', title ='Login', form=form)
+
+    
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+
+@app.route("/account")
+@login_required
+def account():
+    return render_template('account.html' , title ='Account')
